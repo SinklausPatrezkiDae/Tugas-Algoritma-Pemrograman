a97fa52e3008f735cec3c9a5f746f9d011ce0117
SinklausPatrezkiDae <85601260+SinklausPatrezkiDae@users.noreply.github.com>
2021-06-09 13:27:58 +0800

    Add files via upload

---
+++ Aplikasi 6 bagian a.py
@@ -0,0 +1,21 @@
+'''
+    program hitung nilai total transaksi printer epson T20
+    created by sinklaus patrezki dae (20083000013)
+    kelas 2A
+'''
+import os 
+clear = lambda : os.system('cls')
+jwb = "y"
+while jwb == "y" or jwb == "t" :
+    clear()
+    print("--- PROGRAM HITUNG NILAI TOTAL TRANSAKSI PRINTER EPSON T20 ---")
+    print("--------------------------------------------------------------")
+    hrg = 660000
+    a = input("Masukkan Jumlah Printer = ")
+    n = int(a)
+    totHrg = n * hrg
+    print("Total Harga = ",format(totHrg,",.2f"))
+    print()
+    jwb = input("Hitung Lagi? (Y/T) = ")
+    if jwb == "t" or jwb == "T":
+        break
